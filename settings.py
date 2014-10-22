86f81210141f513f80d0edfb8157c65a9faf07c2
Wildhoney <adam.timberlake@gmail.com>
2014-10-22 23:17:17 +0100

    Modified config vars

--- settings.py
+++ settings.py
@@ -3,8 +3,8 @@ import os
 DOMAIN = {'people': {}}
 
 # Mongo Configuration
-MONGO_HOST = os.environ["MONGOHQ_PATH"]
-MONGO_PORT = 27017
+MONGO_HOST = os.environ["MONGOHQ_URL"]
+MONGO_PORT = os.environ["MONGOHQ_PORT"]
 MONGO_USERNAME = os.environ["MONGOHQ_USER"]
 MONGO_PASSWORD = os.environ["MONGOHQ_PASS"]
-MONGO_DBNAME = 'recipe'
+MONGO_DBNAME = os.environ["MONGOHQ_DBNAME"]
