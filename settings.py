bd5165571a019d93e9cbb977e6e72e30305f537b
Wildhoney <adam.timberlake@gmail.com>
2014-10-22 23:32:01 +0100

    Changed Eve settings

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 import os
 
-DOMAIN = {'people': {}}
+# Service Configuration
+SERVER_NAME = "localhost:%d" % int(os.environ["PORT"])
 
 # Mongo Configuration
 MONGO_HOST = os.environ["MONGOHQ_HOST"]
@@ -8,3 +9,6 @@ MONGO_PORT = os.environ["MONGOHQ_PORT"]
 MONGO_USERNAME = os.environ["MONGOHQ_USER"]
 MONGO_PASSWORD = os.environ["MONGOHQ_PASS"]
 MONGO_DBNAME = os.environ["MONGOHQ_DBNAME"]
+
+# API Configuration
+DOMAIN = {'people': {}}
