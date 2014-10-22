d02c5c52d84c28a8dee04a5de61fca8fc17aca38
Wildhoney <adam.timberlake@gmail.com>
2014-10-22 23:09:45 +0100

    Added MongoDB env vars

--- settings.py
+++ settings.py
@@ -1,8 +1,10 @@
+import os
+
 DOMAIN = {'people': {}}
 
 # Mongo Configuration
-# MONGO_HOST = 'localhost'
-# MONGO_PORT = 27017
-# # MONGO_USERNAME = 'user'
-# # MONGO_PASSWORD = 'user'
-# MONGO_DBNAME = 'recipe'
+MONGO_HOST = os.environ["MONGOHQ_PATH"]
+MONGO_PORT = 27017
+MONGO_USERNAME = os.environ["MONGOHQ_USER"]
+MONGO_PASSWORD = os.environ["MONGOHQ_PASS"]
+MONGO_DBNAME = 'recipe'
