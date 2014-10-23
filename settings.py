34059d6f894db308de013ba695ff9bd354eb4696
Wildhoney <adam.timberlake@gmail.com>
2014-10-23 09:36:38 +0100

    Modified Heroku configuration

--- settings.py
+++ settings.py
@@ -1,26 +1,26 @@
 import os
 
-if os.environ.get("PORT"):
+if os.environ.get('PORT'):
 
     # Service Configuration
-    SERVER_NAME = "%s:%s" % (os.environ.get("HOST"), os.environ.get("PORT"))
+    SERVER_NAME = os.environ.get('HOST')
 
     # Mongo Configuration
-    MONGO_HOST = os.environ.get("MONGOHQ_HOST")
-    MONGO_PORT = int(os.environ.get("MONGOHQ_PORT"))
-    MONGO_USERNAME = os.environ.get("MONGOHQ_USER")
-    MONGO_PASSWORD = os.environ.get("MONGOHQ_PASS")
-    MONGO_DBNAME = os.environ.get("MONGOHQ_DBNAME")
+    MONGO_HOST = os.environ.get('MONGOHQ_HOST')
+    MONGO_PORT = int(os.environ.get('MONGOHQ_PORT'))
+    MONGO_USERNAME = os.environ.get('MONGOHQ_USER')
+    MONGO_PASSWORD = os.environ.get('MONGOHQ_PASS')
+    MONGO_DBNAME = os.environ.get('MONGOHQ_DBNAME')
 
 else:
 
     # Service Configuration
-    SERVER_NAME = "0.0.0.0:3507"
+    SERVER_NAME = '0.0.0.0:3507'
 
     # Mongo Configuration
-    MONGO_HOST = "localhost"
+    MONGO_HOST = 'localhost'
     MONGO_PORT = 27017
-    MONGO_DBNAME = "recipe"
+    MONGO_DBNAME = 'recipe'
 
 
 # API Configuration
