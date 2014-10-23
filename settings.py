9abc4fc77a388da8a2ef34c36c5e28d5a15842c2
Wildhoney <adam.timberlake@gmail.com>
2014-10-23 08:18:36 +0100

    Fixed syntax issue

--- settings.py
+++ settings.py
@@ -3,14 +3,14 @@ import os
 if os.environ.get("PORT"):
 
     # Service Configuration
-    SERVER_NAME = "%s:%s" % os.environ.get("HOST"), os.environ.get("PORT")
+    SERVER_NAME = "%s:%s" % (os.environ.get("HOST", "localhost"), os.environ.get("PORT", 5000))
 
     # Mongo Configuration
-    MONGO_HOST = os.environ.get("MONGOHQ_HOST")
-    MONGO_PORT = int(os.environ.get("MONGOHQ_PORT"))
-    MONGO_USERNAME = os.environ.get("MONGOHQ_USER")
-    MONGO_PASSWORD = os.environ.get("MONGOHQ_PASS")
-    MONGO_DBNAME = os.environ.get("MONGOHQ_DBNAME")
+    # MONGO_HOST = os.environ.get("MONGOHQ_HOST")
+    # MONGO_PORT = int(os.environ.get("MONGOHQ_PORT"))
+    # MONGO_USERNAME = os.environ.get("MONGOHQ_USER")
+    # MONGO_PASSWORD = os.environ.get("MONGOHQ_PASS")
+    # MONGO_DBNAME = os.environ.get("MONGOHQ_DBNAME")
 
 else:
 
