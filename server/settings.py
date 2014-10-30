4e20b55f8396a493aa0e4dde07c7d376913cb7b0
Wildhoney <adam.timberlake@gmail.com>
2014-10-30 17:54:30 +0000

    Refactored Eve settings file

--- server/settings.py
+++ server/settings.py
@@ -1,40 +1,76 @@
+#!/usr/bin/env python
+
 import os
+import pymongo
 
 if os.environ.get('HOST'):
 
     # Service Configuration
     SERVER_NAME = os.environ.get('HOST')
 
-# Mongo Configuration
-MONGO_HOST = os.environ.get('MONGOHQ_HOST', 'lennon.mongohq.com')
-MONGO_PORT = int(os.environ.get('MONGOHQ_PORT', 10042))
-MONGO_USERNAME = os.environ.get('MONGOHQ_USER', 'heroku')
-MONGO_PASSWORD = os.environ.get('MONGOHQ_PASS', '6x-3Jj1DA01q9RkFdM7SazZmahEkqaO1YG_jjTgIm6ZAck3N-Aa6f19uZeZvT8t41Fk_lzGSWZUEwEJOnsltDA')
-MONGO_DBNAME = os.environ.get('MONGOHQ_DBNAME', 'app30926695')
+# Default MongoDB Configuration.
+MONGO_HOST   = 'localhost'
+MONGO_PORT   = 27017
+MONGO_DBNAME = 'recipes'
+
+# Parse the `MONGOHQ_URL` variables into the required Eve constants.
+host = os.environ.get('MONGOHQ_URL', 'mongodb://%s:%d' % (MONGO_HOST, MONGO_PORT))
+
+if host is not None:
+
+    host = pymongo.uri_parser.parse_uri(host)
+    port = int(host['nodelist'][0][1])
+
+    # Mongo Configuration
+    MONGO_HOST = host['nodelist'][0][0]
+    MONGO_PORT = port if port else MONGO_PORT
+
+    if host['database']:
+        MONGO_DBNAME = host['database']
+
+    if host['username']:
+        MONGO_USERNAME = host['username']
+
+    if host['password']:
+        MONGO_PASSWORD = host['password']
+
+# MongoDB Debug Output.
+print " * Using MongoDB: mongodb://%s:%d/%s" % (MONGO_HOST, MONGO_PORT, MONGO_DBNAME)
+
+if host['username'] and host['password']:
+
+    # MongoDB Credentials
+    print " * Credentials: %s:%s" % (host['username'], host['password'])
 
 # Schema Details
-schema_recipes = {
-
-    'name': {
-        'type': 'string',
-        'minlength': 3,
-        'maxlength': 32,
-        'required': True,
-        'unique': True
-    },
-    'ingredients': {
-        'type': 'list',
-        'required': True
-    }
-
-}
-
 recipes = {
-    'schema': schema_recipes,
+
     'resource_methods': ['GET', 'POST'],
-    'item_methods': ['GET', 'DELETE']
+    'item_methods':     ['GET', 'DELETE'],
+    'schema': {
+        'name': {
+            'type': 'string',
+            'minlength': 3,
+            'maxlength': 32,
+            'required': True,
+            'unique': True
+        },
+        'description': {
+            'type': 'string',
+            'required': False,
+            'unique': False
+        },
+        'ingredients': {
+            'type': 'list',
+            'required': True,
+            'unique': False
+        }
+    }
 }
 
 # API Configuration
 DOMAIN    = {'recipes': recipes}
 X_DOMAINS = '*'
+IF_MATCH  = False
+
+print " * ---"
